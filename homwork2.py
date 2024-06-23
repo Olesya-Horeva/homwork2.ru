53f378f9bacdb86a5d41e963e27a5d67290172cc
Olesya-Horeva <kushnerova.olesya.1994@mail.ru>
2024-06-24 01:42:23 +0300

    Add files via upload

---
+++ homwork2.py
@@ -0,0 +1,6 @@
+number_of_tasks = '12,'
+number_of_hours = '1.5,'
+course_name = 'Python,'
+time_for_one_task = '0,125'
+print("Курс:",course_name,'всего задач:',number_of_tasks,'затрачено часов:',number_of_hours,'среднее время выполнения')
+print(time_for_one_task,'часа')
